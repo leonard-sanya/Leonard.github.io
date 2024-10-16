032c75a0a8a782f9e94e606260367e6b2f9afc3d
Leonard <lsanya@aimsammi.org>
2024-10-16 01:37:32 +0000

    added new buttons

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def bio():
     
     """)
 def cv():
+    set_background('images/img.png') 
     st.title("Curriculum Vitae")
     with st.expander("📁 Education"):
         st.markdown("Here are some of my recent works:")
